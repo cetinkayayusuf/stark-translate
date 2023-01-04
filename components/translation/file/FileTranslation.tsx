31da1d62713d99d04b85fe983491f925c3b9ab39
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-01-04 10:20:15 +0300

    Add No Target Language and Translation Fail Error Handling for File Translation

--- components/translation/file/FileTranslation.tsx
+++ components/translation/file/FileTranslation.tsx
@@ -1,17 +1,16 @@
 import React, {FunctionComponent, useState} from 'react';
-import {Button, HStack, Input, VStack} from "@chakra-ui/react";
+import {Button, HStack, Input, useToast, VStack} from "@chakra-ui/react";
 import {LanguageSelection} from "../../common/LanguageSelection";
 import {
     TranslationFileInputType,
     TranslationFileOutputType,
-    TranslationTextInputType
 } from "../../../lib/translation/types";
-import {translateText} from "../../../lib/translation";
 import {parseCsvFile} from "../../../lib/parseCsvFile";
 import {translateFile} from "../../../lib/translation/translateFile";
 
 type FileTranslationProps = {}
 export const FileTranslation: FunctionComponent<FileTranslationProps> = ({}) => {
+    const toast = useToast();
     const [translationInput, setTranslationInput] = useState<TranslationFileInputType>({
         file: [],
         source: "",
@@ -36,24 +35,62 @@ export const FileTranslation: FunctionComponent<FileTranslationProps> = ({}) => 
         link.href = url;
         link.click();
     }
-    const handleTranslate = (): void => {
+    const handleTranslate = async (): Promise<void> => {
+        if (translationInput.target === '') {
+            toast({
+                title: 'No Target Language Selected',
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            });
+            return;
+        }
         setTranslationCompleted(false);
-        translateFile(translationInput).then(translationData => {
-            setTranslationOutput(translationData)
-            setTranslationCompleted(true);
-        })
+        try {
+            await translateFile(translationInput).then(translationData => {
+                setTranslationOutput(translationData)
+                setTranslationCompleted(true);
+            })
+        } catch (e) {
+            toast({
+                title: 'Translation Failed',
+                description: (e as Error).message,
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            });
+        }
     }
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         if (event.target.files === null || event.target.files.length === 0) return;
-        parseCsvFile({
-            file: event.target.files[0],
-            onParseComplete: (parsedFile: any) => {
-                setTranslationInput(({
-                    ...translationInput,
-                    file: parsedFile.data
-                } as TranslationFileInputType))
-            }
-        })
+        try {
+            parseCsvFile({
+                file: event.target.files[0],
+                onParseComplete: (parsedFile: any) => {
+                    if (parsedFile.data === null || parsedFile.data.length === 0) {
+                        toast({
+                            title: 'Input File Is Empty',
+                            status: 'error',
+                            duration: 2000,
+                            isClosable: true,
+                        });
+                    } else {
+                        setTranslationInput(({
+                            ...translationInput,
+                            file: parsedFile.data
+                        } as TranslationFileInputType))
+                    }
+                }
+            })
+        } catch (e) {
+            toast({
+                title: 'Cant Parse Input File',
+                description: (e as Error).message,
+                status: 'error',
+                duration: 2000,
+                isClosable: true,
+            });
+        }
 
     }
     const handleSourceLanguageSelect = (selectedLanguage: string): void => {
